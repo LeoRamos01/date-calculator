4219b9bd6bca0e60be02e6b42da4989833076ab5
Leonardo Ramos <leonardo.ramos-ext@viavarejo.com.br>
2020-08-31 11:12:04 -0300

    First commit

---
+++ src/test/java/lramos/datecalculator/DateCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package lramos.datecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
